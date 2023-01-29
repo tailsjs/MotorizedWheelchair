9671b83874be0e97e98f296397b4bfb52434ec28
тейлс <49571184+tailsjs@users.noreply.github.com>
2023-01-29 21:47:31 +0400

    da

--- main.js
+++ main.js
@@ -1,41 +1,47 @@
-let isOn = false;
+class MotorizedWheelchair{
+    constructor(){
+        this.isOn = false;
+    }
 
-function turnOn() {
-  if (!isOn) {
-    isOn = true;
-    console.log("Wheelchair turned on");
-  } else {
-    console.log("Wheelchair is already on");
-  }
+    turnOn(){
+        if(!this.isOn){
+            this.isOn = true;
+            console.log("Wheelchair turned on")
+        }else{
+            console.log("Wheelchair is already on")
+        }
+    }
+
+    turnOff(){
+        if(this.isOn){
+            this.isOn = false;
+            console.log("Wheelchair turned on")
+        }else{
+            console.log("Wheelchair is already on")
+        }
+    }
+
+    moveForward(){
+        if(this.isOn){
+            console.log("Wheelchair moving forward")
+        } else {
+            console.log("Turn on the wheelchair first")
+        }
+    }
+
+    moveBackward(){
+        if(this.isOn){
+            console.log("Wheelchair moving forward")
+        } else {
+            console.log("Turn on the wheelchair first")
+        }
+    }
 }
 
-function turnOff() {
-  if (isOn) {
-    isOn = false;
-    console.log("Wheelchair turned off");
-  } else {
-    console.log("Wheelchair is already off");
-  }
-}
+let wheelchair = new MotorizedWheelchair()
 
-function moveForward() {
-  if (isOn) {
-    console.log("Wheelchair moving forward");
-  } else {
-    console.log("Turn on the wheelchair first");
-  }
-}
-
-function moveBackward() {
-  if (isOn) {
-    console.log("Wheelchair moving backward");
-  } else {
-    console.log("Turn on the wheelchair first");
-  }
-}
-
-turnOn();
-moveForward();
-moveBackward();
-turnOff();
-moveForward();
+wheelchair.turnOn()
+wheelchair.moveForward()
+wheelchair.moveBackward()
+wheelchair.turnOff()
+wheelchair.moveForward()
